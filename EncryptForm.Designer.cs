4eec2d8184c6a5a67baaac39d0ef2b973eb3451f
huytht <huytht2001@gmail.com>
2022-03-23 14:18:51 +0700

    commit second time

--- EncryptForm.Designer.cs
+++ EncryptForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace EncryptAlgorithm
 {
-    partial class Form1
+    partial class EncryptForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,17 @@ namespace EncryptAlgorithm
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // EncryptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1063, 565);
+            this.Name = "EncryptForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- EncryptForm.cs
+++ EncryptForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EncryptAlgorithm
 {
-    public partial class Form1 : Form
+    public partial class EncryptForm : Form
     {
-        public Form1()
+        public EncryptForm()
         {
             InitializeComponent();
         }
