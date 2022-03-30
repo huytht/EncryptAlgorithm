53f6b166893e44002e1743afbcc80ebd99ae20d1
huytht <huytht2001@gmail.com>
2022-03-30 07:54:53 +0700

    add validate for all field

--- EncryptForm.cs
+++ EncryptForm.cs
@@ -17,21 +17,49 @@ namespace EncryptAlgorithm
         Vigener vigener = new Vigener(String.Empty);
         public EncryptForm()
         {
+            this.StartPosition = FormStartPosition.CenterScreen;
             InitializeComponent();
         }
 
         private void btnEncryptVigener_Click(object sender, EventArgs e)
-        {
-            vigener.key = txtKeyVigener.Text;
-            vigener.plainText = txtPlainTextVigener.Text;
-            txtCipherVigener.Text = vigener.encryptVietnamese();
+        {   
+            if (txtKeyVigener.Text.Length > 0 && txtKeyVigener.Text.Trim() != "")
+            {
+                vigener.key = txtKeyVigener.Text;
+                if (txtPlainTextVigener.Text.Length > 0 && txtPlainTextVigener.Text.Trim() != "")
+                {
+                    vigener.plainText = txtPlainTextVigener.Text;
+                    txtCipherVigener.Text = vigener.encryptVietnamese();
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng nhập chuỗi cần mã hóa");
+                }
+            } else
+            {
+                MessageBox.Show("Vui lòng nhập khóa");
+            }
         }
 
         private void btnDecryptVigener_Click(object sender, EventArgs e)
         {
-            vigener.key = txtKeyVigener.Text;
-            vigener.cipherText = txtCipherVigener.Text;
-            txtDecryptVigener.Text = vigener.decryptVietnamese();
+            if (txtKeyVigener.Text.Length > 0 && txtKeyVigener.Text.Trim() != "")
+            {
+                vigener.key = txtKeyVigener.Text;
+                if (txtCipherVigener.Text.Length > 0 && txtCipherVigener.Text.Trim() != "")
+                {
+                    vigener.cipherText = txtCipherVigener.Text;
+                    txtDecryptVigener.Text = vigener.decryptVietnamese();
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng nhập chuỗi đã mã hóa");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập khóa");
+            }
         }
 
         #region BienTrungGian
@@ -42,18 +70,33 @@ namespace EncryptAlgorithm
 
         private void btnEncryptRSA_Click(object sender, EventArgs e)
         {
-            plainTextRSA = Encoding.UTF8.GetBytes(txtPlainTextRSA.Text);
-            cipherTextRSA = RSAEncryption.Encrypt(plainTextRSA, rsa.ExportParameters(false), false);
-            StringBuilder sbHash = new StringBuilder();
-            foreach (byte b in cipherTextRSA)
-                sbHash.Append(String.Format("{0:x2}", b));
-            txtCipherTextRSA.Text = sbHash.ToString();
+            if (txtPlainTextRSA.Text.Length > 0 && txtPlainTextRSA.Text.Trim() != "")
+            {
+                plainTextRSA = Encoding.UTF8.GetBytes(txtPlainTextRSA.Text);
+                cipherTextRSA = RSAEncryption.Encrypt(plainTextRSA, rsa.ExportParameters(false), false);
+                StringBuilder sbHash = new StringBuilder();
+                foreach (byte b in cipherTextRSA)
+                    sbHash.Append(String.Format("{0:x2}", b));
+                txtCipherTextRSA.Text = sbHash.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập chuỗi cần mã hóa");
+            }
+
         }
 
         private void btnDecryptRSA_Click(object sender, EventArgs e)
         {
-            byte[] decryptText = RSAEncryption.Decrypt(cipherTextRSA, rsa.ExportParameters(true), false);
-            txtDecryptTextRSA.Text = Encoding.UTF8.GetString(decryptText);
+            if (txtCipherTextRSA.Text.Length > 0 && txtCipherTextRSA.Text.Trim() != "")
+            {
+                byte[] decryptText = RSAEncryption.Decrypt(cipherTextRSA, rsa.ExportParameters(true), false);
+                txtDecryptTextRSA.Text = Encoding.UTF8.GetString(decryptText);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập chuỗi đã mã hóa");
+            }
         }
     }
 }
